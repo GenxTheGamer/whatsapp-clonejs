4751e24b28eca4bef0ba149285e20ab98729415b
Vaibhav Vyas <52570863+GenxTheGamer@users.noreply.github.com>
2021-04-21 09:37:49 +0530

    Chat.js Added | Fixed Avatar

--- src/Chat.js
+++ src/Chat.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import { Avatar, IconButton } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
 import "./Chat.css";
-
+import ChatIcon from "@material-ui/icons/Chat";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import { AttachFile, SearchOutlined } from "@material-ui/icons";
 
 function Chat() {
-    return (
-        <div className="chat">
-            
+  const [seed, setSeed] = useState("");
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last Seen ...</p>
         </div>
-    )
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        <p className={`chat__message ${true && "chat__reciever"}`}>
+          <span className="chat__name">Genx</span>
+          Hey!!!!!!!!!
+          <span className="chat__timestamp">10:15PM</span>
+        </p>
+        <p className="chat__message">Hey!!!!!!!!!</p>
+      </div>
+
+      <div className="chat__footer"></div>
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
